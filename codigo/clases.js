beeb698a0757d4d75031b3506469455686bd7a58
wiredmatt <29718978+wiredmatt@users.noreply.github.com>
2023-10-26 14:14:41 -0300

    styling

--- codigo/clases.js
+++ codigo/clases.js
@@ -1,33 +1,41 @@
 class Pelicula {
-    constructor(pId, pNombre, pAnio, pGenero, pNumeroVotantes, pTotalPuntos) {
-        this.id = pId;
-        this.nombre = pNombre;
-        this.anio = pAnio;
-        this.genero = pGenero;
-        this.numeroVotanes = pNumeroVotantes;
-        this.totalPuntos = pTotalPuntos;
-    }
+  constructor(pId, pNombre, pAnio, pGenero, pNumeroVotantes, pTotalPuntos) {
+    this.id = pId;
+    this.nombre = pNombre;
+    this.anio = pAnio;
+    this.genero = pGenero;
+    this.numeroVotanes = pNumeroVotantes;
+    this.totalPuntos = pTotalPuntos;
+  }
 }
 
 class UsuarioAdministrador {
-    constructor(pNombreUsuario, pContrasena) {
-        this.nombreUsuario = pNombreUsuario;
-        this.contrasena = pContrasena;
-    }
+  constructor(pNombreUsuario, pContrasena) {
+    this.nombreUsuario = pNombreUsuario;
+    this.contrasena = pContrasena;
+  }
 }
 
 class UsuarioComun {
-    static contadorID = 1;
+  static contadorID = 1;
 
-    constructor(pNombre, pApellido, pNomUsu, pContrasena, pNroTjt, pCvcTjt, pEstadoUsuario) {
-        this.ID = UsuarioComun.contadorID;
-        this.nombre = pNombre;
-        this.apellido = pApellido;
-        this.nombreUsuario = pNomUsu;
-        this.contrasena = pContrasena;
-        this.nroTarjeta = pNroTjt;
-        this.cvcTarjeta = pCvcTjt;
-        this.estado = pEstadoUsuario;
-        UsuarioComun.contadorID++;
-    }
+  constructor(
+    pNombre,
+    pApellido,
+    pNomUsu,
+    pContrasena,
+    pNroTjt,
+    pCvcTjt,
+    pEstadoUsuario
+  ) {
+    this.ID = UsuarioComun.contadorID;
+    this.nombre = pNombre;
+    this.apellido = pApellido;
+    this.nombreUsuario = pNomUsu;
+    this.contrasena = pContrasena;
+    this.nroTarjeta = pNroTjt;
+    this.cvcTarjeta = pCvcTjt;
+    this.estado = pEstadoUsuario;
+    UsuarioComun.contadorID++;
+  }
 }
