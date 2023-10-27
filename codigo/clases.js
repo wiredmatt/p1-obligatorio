da9f44a6f5bec3863b9e4fe800d08fdfce32eea8
wiredmatt <29718978+wiredmatt@users.noreply.github.com>
2023-10-27 01:42:33 -0300

    tarea(clases): jsdoc

--- codigo/clases.js
+++ codigo/clases.js
@@ -1,6 +1,22 @@
+/**
+ * @typedef {("c7.small" | "c7.medium" | "c7.large" | "r7.small" | "r7.medium" | "r7.large" | "i7.medium" | "i7.large")} INSTANCIA_TIPO
+ *
+ * @typedef {("Optimizadas para computo" | "Optimizadas para memoria" | "Optimizadas para almacenamiento")} INSTANCIA_CATEGORIA
+ */
+
 class UsuarioAdministrador {
+  /**
+   * @param {string} pNombreUsuario
+   * @param {string} pContrasena
+   */
   constructor(pNombreUsuario, pContrasena) {
+    /**
+     * @type {string}
+     */
     this.nombreUsuario = pNombreUsuario;
+    /**
+     * @type {string}
+     */
     this.contrasena = pContrasena;
   }
 }
@@ -8,13 +24,42 @@ class UsuarioAdministrador {
 class UsuarioComun {
   static contadorID = 1;
 
+  /**
+   * @param {string} pNombre
+   * @param {string} pApellido
+   * @param {string} pNomUsu
+   * @param {string} pContrasena
+   * @param {string} pNroTjt
+   * @param {number} pCvcTjt
+   */
   constructor(pNombre, pApellido, pNomUsu, pContrasena, pNroTjt, pCvcTjt) {
+    /**
+     * @type {number}
+     */
     this.ID = UsuarioComun.contadorID;
+    /**
+     * @type {string}
+     */
     this.nombre = pNombre;
+    /**
+     * @type {string}
+     */
     this.apellido = pApellido;
+    /**
+     * @type {string}
+     */
     this.nombreUsuario = pNomUsu;
+    /**
+     * @type {string}
+     */
     this.contrasena = pContrasena;
+    /**
+     * @type {string}
+     */
     this.nroTarjeta = pNroTjt;
+    /**
+     * @type {string}
+     */
     this.cvcTarjeta = pCvcTjt;
 
     /**
@@ -30,58 +75,98 @@ class MaquinaVirtual {
   static contadorID = 1;
 
   /**
-   * @param {("c7.small" | "c7.medium" | "c7.large" | "r7.small" | "r7.medium" | "r7.large" | "i7.medium" | "i7.large")} pTipo
+   * @param {INSTANCIA_TIPO} tipo
+   * @returns {INSTANCIA_CATEGORIA}
+   */
+  static tipoACategoria(tipo) {
+    switch (tipo) {
+      case "c":
+        return "Optimizadas para computo";
+      case "r":
+        return "Optimizadas para memoria";
+      case "i":
+        return "Optimizadas para almacenamiento";
+    }
+  }
+
+  /**
+   * @param {INSTANCIA_TIPO} tipo
+   * @returns {number}
+   */
+  static tipoACostoAlquiler(tipo) {
+    switch (tipo) {
+      case "c7.small":
+        return 20;
+      case "c7.medium":
+        return 30;
+      case "c7.large":
+        return 50;
+      case "r7.small":
+        return 35;
+      case "r7.medium":
+        return 50;
+      case "r7.large":
+        return 60;
+      case "i7.medium":
+        return 30;
+      case "i7.large":
+        return 50;
+    }
+  }
+
+  /**
+   * @param {INSTANCIA_TIPO} tipo
+   * @returns {number}
+   */
+  static tipoACostoEncendido(tipo) {
+    switch (tipo) {
+      case "c7.small":
+        return 2.5;
+      case "c7.medium":
+        return 3.5;
+      case "c7.large":
+        return 6.0;
+      case "r7.small":
+        return 4.0;
+      case "r7.medium":
+        return 6.5;
+      case "r7.large":
+        return 7.0;
+      case "i7.medium":
+        return 3.5;
+      case "i7.large":
+        return 6.5;
+    }
+  }
+
+  /**
+   * @param {INSTANCIA_TIPO} pTipo
    */
   constructor(pTipo) {
-    this.id = MaquinaVirtual.contadorID;
+    /**
+     * @type {INSTANCIA_TIPO}
+     */
     this.tipo = pTipo;
 
-    switch (pCategoria[0]) {
-      case "c":
-        this.categoria = "Optimizadas para computo";
-        break;
-      case "r":
-        this.categoria = "Optimizadas para memoria";
-        break;
-      case "i":
-        this.categoria = "Optimizadas para almacenamiento";
-        break;
-    }
+    /**
+     * @type {number}
+     */
+    this.id = MaquinaVirtual.contadorID;
 
-    switch (pTipo) {
-      case "c7.small":
-        this.costoAlquiler = 20;
-        this.costoEncendido = 2.5;
-        break;
-      case "c7.medium":
-        this.costoAlquiler = 30;
-        this.costoEncendido = 3.5;
-        break;
-      case "c7.large":
-        this.costoAlquiler = 50;
-        this.costoEncendido = 6.0;
-        break;
-      case "r7.small":
-        this.costoAlquiler = 35;
-        this.costoEncendido = 4.0;
-        break;
-      case "r7.medium":
-        this.costoAlquiler = 50;
-        this.costoEncendido = 6.5;
-        break;
-      case "r7.large":
-        this.costoAlquiler = 60;
-        this.costoEncendido = 7.0;
-        break;
-      case "i7.medium":
-        this.costoAlquiler = 30;
-        this.costoEncendido = 3.5;
-        break;
-      case "i7.large":
-        this.costoAlquiler = 50;
-        this.costoEncendido = 6.5;
-        break;
-    }
+    /**
+     * @type {INSTANCIA_CATEGORIA}
+     */
+    this.categoria = MaquinaVirtual.tipoACategoria(pTipo);
+
+    /**
+     * @type {number}
+     */
+    this.costoAlquiler = MaquinaVirtual.tipoACostoAlquiler(pTipo);
+
+    /**
+     * @type {number}
+     */
+    this.costoEncendido = MaquinaVirtual.tipoACostoEncendido(pTipo);
 
     /**
      * @type {("apagada" | "encendida")}
@@ -93,6 +178,9 @@ class MaquinaVirtual {
     MaquinaVirtual.contadorID++;
   }
 
+  /**
+   * @returns {boolean} false si la maquina ya estaba prendida, true si estaba apagada.
+   */
   encender() {
     if (this.estado !== "apagada") {
       return false;
